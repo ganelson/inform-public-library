4af0bbaba003130250c0f2589d11d0d462917e64
Graham Nelson <graham.nelson@mod-langs.ox.ac.uk>
2022-08-01 21:32:47 +0100

    Reworked CSS and v10 disclaimer text

--- README.md
+++ README.md
@@ -16,26 +16,32 @@ is built like so:
 ```
 inbuild -build-registry inform-public-library/pl-v9/catalogue.txt
 ```
-What this does is to build (currently) four indexing files from templates
+What this does is to build (currently) ten indexing files from templates
 in `pl-v9/source` to produce the indexes into `pl-v9` itself. Specifically,
 it turns:
 
 - `pl-v9/source/index.html` into `pl-v9/index.html`
+- `pl-v9/source/index-linux.html` into `pl-v9/index-linux.html`
+- `pl-v9/source/index-macos.html` into `pl-v9/index-macos.html`
+- `pl-v9/source/index-windows.html` into `pl-v9/index-windows.html`
 - `pl-v9/source/pl404.html` into `pl-v9/pl404.html`
+- `pl-v9/source/pl404-linux.html` into `pl-v9/pl404-linux.html`
+- `pl-v9/source/pl404-macos.html` into `pl-v9/pl404-macos.html`
+- `pl-v9/source/pl404-windows.html` into `pl-v9/pl404-windows.html`
 - `pl-v9/source/catalogue.txt` into `pl-v9/catalogue.txt`
 - `pl-v9/source/headings.txt` into `pl-v9/headings.txt`
 
 The `pl404.html` page is not actually needed on the server, but instead is
 used in the apps, to be displayed if the Internet (or the server) is down.
 
+There are different versions for each app in order to customise the CSS.
+Windows needs different font choices, MacOS does idiosyncratic things to
+make Dark Mode work with the app. The versions with no platform indicated
+are generic in the same way that the v9 Inform Public Library page was generic:
+i.e., looking sort of okay but not ideal across the board.
+
 The `catalogue.txt` and `headings.txt` files are purely for human convenience,
 and aren't needed by either the server or the apps.
 
 The resources in this registry are the final state of the version 9.3 Public
 Library as curated by Mark Musante.
-
-This is a work in progress: next steps are to improve the CSS to work better
-in Dark Mode for MacOS and to be less annoying with fonts on Windows, and to
-address the issue of how to make the PL useful in world where many extensions
-need different versions for Inform v10.1, but where users will still want
-access to the v9.3 PL.
--- pl-v9/source/include/section_buttons.js
+++ pl-v9/source/include/section_buttons.js
@@ -15,21 +15,21 @@
 		var imid = 'button' + itemid;
 		var res = myProject.askInterfaceForLocalVersion(author, title, available);
 		var buttontext = 'DOWNLOAD';
-		var buttonclass = 'buttons uninstalled';
+		var buttonclass = 'registrybuttons registrybuttonuninstalled';
 		if (res == '!') {
-			buttontext = 'BUILT IN'; buttonclass = 'buttons builtin';
+			buttontext = 'BUILT IN'; buttonclass = 'registrybuttons registrybuttonbuiltin';
 			window.sectionExtentI[sid]++;
 			window.subsectionExtentI[ssid]++;
 		}
 		if (res == '=') {
-			buttontext = 'INSTALLED'; buttonclass = 'buttons installed';
+			buttontext = 'INSTALLED'; buttonclass = 'registrybuttons registrybuttoninstalled';
 			window.sectionExtentI[sid]++;
 			window.subsectionExtentI[ssid]++;
 		}
 		if (res == '<') {
 			var loc = myProject.askInterfaceForLocalVersionText(author, title);
 			buttontext = 'UPDATE ' + loc + ' &#9654 ' + available;
-			buttonclass = 'buttons update';
+			buttonclass = 'registrybuttons registrybuttonupdate';
 			window.updateList.push(itemid);
 			window.updateList.push(url);
 			window.updateList.push(title + ' by ' + author);
@@ -40,7 +40,7 @@
 		if (res == '>') {
 			var loc = myProject.askInterfaceForLocalVersionText(author, title);
 			buttontext = 'REVERT TO LIBRARY VERSION (' + loc + ' &#9654 ' + available + ')';
-			buttonclass = 'buttons revert';
+			buttonclass = 'registrybuttons registrybuttonrevert';
 			window.sectionExtentR[sid]++;
 			window.subsectionExtentR[ssid]++;
 		}
@@ -62,61 +62,61 @@
 	function addToIndicator(existing, count, className) {
 		if (count == 0) return existing;
 		if (existing != '') existing = existing + '+';
-		return existing + '<span class="minibuttons ' + className + '">' + count + '</span>';
+		return existing + '<span class="registryminibuttons registryminibuttontext ' + className + '">' + count + '</span>';
 	}
 	function setIndicatorForSection(sid) {
-		var temp = addToIndicator('', window.sectionExtentI[sid], 'installed');
-		temp = addToIndicator(temp, window.sectionExtentD[sid], 'uninstalled');
-		temp = addToIndicator(temp, window.sectionExtentU[sid], 'update');
-		temp = addToIndicator(temp, window.sectionExtentR[sid], 'revert');
+		var temp = addToIndicator('', window.sectionExtentI[sid], 'registrybuttoninstalled');
+		temp = addToIndicator(temp, window.sectionExtentD[sid], 'registrybuttonuninstalled');
+		temp = addToIndicator(temp, window.sectionExtentU[sid], 'registrybuttonupdate');
+		temp = addToIndicator(temp, window.sectionExtentR[sid], 'registrybuttonrevert');
 		document.getElementById('sectionindicator'+sid).innerHTML = temp;
 	}
 	function setIndicatorForSubsection(sid) {
-		var temp = addToIndicator('', window.subsectionExtentI[sid], 'installed');
-		temp = addToIndicator(temp, window.subsectionExtentD[sid], 'uninstalled');
-		temp = addToIndicator(temp, window.subsectionExtentU[sid], 'update');
-		temp = addToIndicator(temp, window.subsectionExtentR[sid], 'revert');
+		var temp = addToIndicator('', window.subsectionExtentI[sid], 'registrybuttoninstalled');
+		temp = addToIndicator(temp, window.subsectionExtentD[sid], 'registrybuttonuninstalled');
+		temp = addToIndicator(temp, window.subsectionExtentU[sid], 'registrybuttonupdate');
+		temp = addToIndicator(temp, window.subsectionExtentR[sid], 'registrybuttonrevert');
 		document.getElementById('subsectionindicator'+sid).innerHTML = temp;
 	}
 	function openSection(sid) {
-		document.getElementById('sectionitems'+sid).className = 'sectionitemsopen';
+		document.getElementById('sectionitems'+sid).className = 'registrysectionitemsopen';
 		document.getElementById('sectionindicator'+sid).innerHTML = '';
 	}
 	function closeSection(sid) {
-		document.getElementById('sectionitems'+sid).className = 'sectionitemsclosed';
+		document.getElementById('sectionitems'+sid).className = 'registrysectionitemsclosed';
 		setIndicatorForSection(sid);
 	}
 	function maybeCloseSection(sid, updating) {
-		if ((updating == 1) && (document.getElementById('sectionitems'+sid).className != 'sectionitemsclosed')) {
+		if ((updating == 1) && (document.getElementById('sectionitems'+sid).className != 'registrysectionitemsclosed')) {
 			return;
 		}
-		document.getElementById('sectionitems'+sid).className = 'sectionitemsclosed';
+		document.getElementById('sectionitems'+sid).className = 'registrysectionitemsclosed';
 		setIndicatorForSection(sid);
 	}
 	function toggleSection(sid) {
-		if (document.getElementById('sectionitems'+sid).className == 'sectionitemsclosed') {
+		if (document.getElementById('sectionitems'+sid).className == 'registrysectionitemsclosed') {
 			openSection(sid);
 		} else {
 			closeSection(sid);
 		}
 	}
 	function openSubsection(ssid) {
-		document.getElementById('subsectionitems'+ssid).className = 'sectionitemsopen';
+		document.getElementById('subsectionitems'+ssid).className = 'registrysectionitemsopen';
 		document.getElementById('subsectionindicator'+ssid).innerHTML = '';
 	}
 	function closeSubsection(ssid) {
-		document.getElementById('subsectionitems'+ssid).className = 'sectionitemsclosed';
+		document.getElementById('subsectionitems'+ssid).className = 'registrysectionitemsclosed';
 		setIndicatorForSubsection(ssid);
 	}
 	function maybeCloseSubsection(ssid, updating) {
-		if ((updating == 1) && (document.getElementById('subsectionitems'+ssid).className != 'sectionitemsclosed')) {
+		if ((updating == 1) && (document.getElementById('subsectionitems'+ssid).className != 'registrysectionitemsclosed')) {
 			return;
 		}
-		document.getElementById('subsectionitems'+ssid).className = 'sectionitemsclosed';
+		document.getElementById('subsectionitems'+ssid).className = 'registrysectionitemsclosed';
 		setIndicatorForSubsection(ssid);
 	}
 	function toggleSubsection(ssid) {
-		if (document.getElementById('subsectionitems'+ssid).className == 'sectionitemsclosed') {
+		if (document.getElementById('subsectionitems'+ssid).className == 'registrysectionitemsclosed') {
 			openSubsection(ssid);
 		} else {
 			closeSubsection(ssid);
@@ -125,25 +125,25 @@
 	function updateButtonLabels() {
 		if (window.getAllLength == 0) {
 			document.getElementById('getall').innerHTML = 'ALL DOWNLOADED';
-			document.getElementById('getall').className = 'buttons installed';
+			document.getElementById('getall').className = 'registrybuttons registrybuttoninstalled';
 		} else {
 			if (window.getAllLength == 1) {
 				document.getElementById('getall').innerHTML = 'DOWNLOAD 1 EXTENSION';
 			} else {
 				document.getElementById('getall').innerHTML = 'DOWNLOAD ' + window.getAllLength + ' EXTENSIONS';
 			}
-			document.getElementById('getall').className = 'buttons uninstalled';
+			document.getElementById('getall').className = 'registrybuttons registrybuttonuninstalled';
 		}
 		if (window.updateListLength == 0) {
 			document.getElementById('updateall').innerHTML = 'ALL UP TO DATE';
-			document.getElementById('updateall').className = 'buttons installed';
+			document.getElementById('updateall').className = 'registrybuttons registrybuttoninstalled';
 		} else {
 			if (window.updateListLength == 1) {
 				document.getElementById('updateall').innerHTML = 'UPDATE 1 EXTENSION';
 			} else {
 				document.getElementById('updateall').innerHTML = 'UPDATE ' + window.updateListLength + ' EXTENSIONS';
 			}
-			document.getElementById('updateall').className = 'buttons update';
+			document.getElementById('updateall').className = 'registrybuttons registrybuttonupdate';
 		}
 	}
 	function clearSectionExtents(sid) {
